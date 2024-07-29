9de8c7f3bef77e9e26c3a284281c0044809bb21d
Everaldo-Martins <everaldoinfortecnico@gmail.com>
2024-07-29 10:20:00 -0300

    Atualização do form search

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const h = new Date().getHours();
     const menuItems = document.querySelectorAll(".nav > li > span");
     const dropdown = document.querySelectorAll(".nav-content");
+    const search = document.querySelector('.search');
 
     const closeAll = () => {
         menuItems.forEach((item, index) => {
@@ -25,21 +26,20 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 
-    document.querySelectorAll(".menu_se ul li").forEach((item, index) => {
+    document.querySelectorAll(".menu_se ul li").forEach((item, index) => {        
         item.addEventListener("click", () => {
             document.querySelectorAll(".menu_se ul li").forEach(i => i.classList.remove("active"));
             item.classList.add("active");
-            let search = document.querySelector('.search');
 
-            document.querySelector('.q').name = index === 2 ? "search_query": "q";
+            document.querySelector('.q').name = index === 2 ? "search_query": "q";            
 
             search.action = index === 1 ? "https://www.google.com.br/images" :
                 index === 2 ? "https://www.youtube.com/results" :
-                    "https://www.google.com.br/search"; 
-
-            search.target = "_blank";
-            search.addEventListener("submit", () => setTimeout(() => search.reset(), 100));         
+                    "https://www.google.com.br/search";  
         });
+
+        search.target = "_blank";
+        search.addEventListener("submit", () => setTimeout(() => search.reset(), 100));
     });
 
     document.querySelector('.hello').textContent = h < 12 ? "Olá, seja bem vindo(a)! Tenha um bom dia." :
